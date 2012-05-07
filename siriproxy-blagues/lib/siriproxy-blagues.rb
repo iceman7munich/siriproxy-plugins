39bf85a8d7ae86c9a3cf5aedccc70bbd8c3d572d
Cedric Boverie <ced@boverie.eu>
2012-05-08 00:24:02 +0200

    Message d'erreur pour chuck norris. Jusqu'à ce que le script de téléchargement fonctionne

--- siriproxy-blagues/lib/siriproxy-blagues.rb
+++ siriproxy-blagues/lib/siriproxy-blagues.rb
@@ -41,16 +41,16 @@ class SiriProxy::Plugin::Blagues < SiriProxy::Plugin
 	end
 	
 	listen_for /chuck norris/i do
-		#begin
+		begin
 			file = File.open(@dir+"/fortunes-cn", "r:UTF-8")
 			contents = file.read
 			liste = contents.split('%')
 			rand = rand(liste.length-1)
 			dtc = liste[rand].strip
 			say dtc
-		#rescue
-		#	say "Chuck Norris n'a pas besoin d'une liste de blagues pour être drôle. C'est la liste des blagues qui a besoin de Chuck Norris."
-		#end
+		rescue
+			say "Chuck Norris n'a pas besoin d'une liste de blagues pour être drôle. C'est la liste des blagues qui a besoin de Chuck Norris."
+		end
 		request_completed
 	end
 	
